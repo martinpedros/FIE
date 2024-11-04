01c4d39f59fd43ec9d02381371e6499e688fd356
Martin Pedros <martinpedros@gmail.com>
2024-11-04 20:29:45 -0300

    Create pendiente.py

---
+++ 0_Curso_Matematica/pendiente.py
@@ -0,0 +1,22 @@
+# ###################################################################################
+#
+# Martin Pedros
+#
+# Dados los puntos P1(x0, y0) y P2(x1, y1), calcular la pendiente de una recta:
+# 
+# pendiente = (y1 - y0) / (x1 - x0)
+#
+# ###################################################################################
+
+def pendiente(x0, y0, x1, y1):
+    return ((y1 - y0) / (x1 - x0))
+
+x0 = int(input('Ingresar el valor de x del punto P1 (x0): '))
+y0 = int(input('Ingresar el valor de y del punto P1 (y0): '))
+x1 = int(input('Ingresar el valor de x del punto P2 (x1): '))
+y1 = int(input('Ingresar el valor de y del punto P2 (y1): '))
+
+print(f'\nEl punto P1(x0, y0) es: ({x0},{y0})')
+print(f'\nEl punto P2(x1, y1) es: ({x1},{y1})')
+
+print(f'\nLa pendiente "a" es: {pendiente(x0, y0, x1, y1)}\n')
