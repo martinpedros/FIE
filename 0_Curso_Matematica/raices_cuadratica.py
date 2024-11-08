0ec44c91087ed4544928458214c04a3ecf654d76
Martin Pedros <martinpedros@gmail.com>
2024-11-08 20:21:02 -0300

    Update raices_cuadratica.py

--- 0_Curso_Matematica/raices_cuadratica.py
+++ 0_Curso_Matematica/raices_cuadratica.py
@@ -1,11 +1,25 @@
+# ###################################################################################
+#
+# Martin Pedros
+# Dados los coeficientes de una funcion cuadratica: a, b y c; calcula las raices si
+# existen: f(x) = a*x^2 + b*x + c
+#
+# ###################################################################################
+
 def raiz(a, b, c):
     return ((-b+(b**2-4*a*c)**(1/2))/2*a)
 
-a = int(input('Ingrese coeficiente cuadratico: '))
+print('\n##################################################')
+print('Calculo de raices de una Parabola')
+print('##################################################')
+
+a = int(input('\nIngrese coeficiente cuadratico: '))
 b = int(input('Ingrese coeficiente lineal: '))
 c = int(input('Ingrese termino independiente: '))
 
-if ((b**2-4*a*c) > 0):
-    print(f'Una raiz de a(x): {raiz(a, b, c)}')
+if ((b**2-4*a*c) >= 0):
+    print(f'\nUna raiz de f(x) = a*(x)^2 + b*x + c: {raiz(a, b, c)}\n')
 else:
-    print(f'No hay raices reales')
+    print('\n##################################################')
+    print('No hay raices reales')
+    print('##################################################\n')
