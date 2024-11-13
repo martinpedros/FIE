dc74116fba6ea814ad0b5d1c13efdff58d6c28a3
Martin Pedros <martinpedros@gmail.com>
2024-11-13 16:48:22 -0300

    Update pendiente_recta.py

--- 0_Curso_Matematica/pendiente_recta.py
+++ 0_Curso_Matematica/pendiente_recta.py
@@ -1,12 +1,24 @@
 # ###################################################################################
 #
-# Martin Pedros
-# Dados los puntos P1(x0, y0) y P2(x1, y1), calcular la pendiente de una recta:
-# pendiente = (y1 - y0) / (x1 - x0)
+# FIE (Facultad de Ingenieria del Ejercito)
+# Materia: Taller de Introducción a la Matemática Universitaria
+# Alumno: Martin Pedros
+# Programa: Cálculo de la pendiente de una Recta
 #
 # ###################################################################################
 
 def pendiente(x0, y0, x1, y1):
+    """Calcula las pendiente de una Recta
+    
+    Dados los puntos P1(x0, y0) y P2(x1, y1), calcular la pendiente de una recta:
+    pendiente = (y1 - y0) / (x1 - x0)
+
+    Args:
+        x0 (int): valor de x del punto P1
+        y0 (int): valor de y del punto P1
+        x1 (int): valor de x del punto P2
+        y1 (int): valor de y del punto P2
+    """
     return ((y1 - y0) / (x1 - x0))
 
 x0 = int(input('Ingresar el valor de x del punto P1 (x0): '))
@@ -14,7 +26,7 @@ y0 = int(input('Ingresar el valor de y del punto P1 (y0): '))
 x1 = int(input('Ingresar el valor de x del punto P2 (x1): '))
 y1 = int(input('Ingresar el valor de y del punto P2 (y1): '))
 
-print('##################################################')
+print('\n##################################################')
 print('Los puntos ingresados son')
 print('##################################################')
 
