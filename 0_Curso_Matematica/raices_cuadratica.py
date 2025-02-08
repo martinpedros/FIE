ae8beaba1dae59b5fea12fb842be563a3f3f4858
Martin Pedros <martinpedros@gmail.com>
2025-02-08 11:30:53 -0300

    Update raices_cuadratica.py

--- 0_Curso_Matematica/raices_cuadratica.py
+++ 0_Curso_Matematica/raices_cuadratica.py
@@ -1,12 +1,16 @@
 # ###################################################################################
 #
-# FIE (Facultad de Ingenieria del Ejercito)
+# Facu
 # Materia: Taller de Introducción a la Matemática Universitaria
 # Alumno: Martin Pedros
 # Programa: Cálculo de raíces de una Parabola
 #
 # Version: 1.0
 # Fecha: 13/11/2024
+#
+# NOTA: Por el momento el programa no tienen deteccion de errores, por lo que los
+# datos se deben ingresar correctamente, de lo contrario se debera volver a ejecutar
+# dicho programa.
 #
 # ###################################################################################
 
