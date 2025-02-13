b1a457cd6eb8b006e673647945f705ef85490578
Martin Pedros <martinpedros@gmail.com>
2025-02-12 23:38:09 -0300

    Update sistemas_lineales_determinantes_2x2.py

--- 0_Curso_Matematica/sistemas_lineales_determinantes_2x2.py
+++ 0_Curso_Matematica/sistemas_lineales_determinantes_2x2.py
@@ -1,16 +1,16 @@
 # ###################################################################################
 #
-# Facu
-# Materia: Taller de Introducción a la Matemática Universitaria
-# Alumno: Martin Pedros
-# Programa: Cálculo de sistemas lineales por método de determinantes de 2x2
+#   Facu
+#   Materia: Taller de Introducción a la Matemática Universitaria
+#   Alumno: Martin Pedros
+#   Programa: Cálculo de sistemas lineales por método de determinantes de 2x2
 #
-# Version: 1.0
-# Fecha: 15/11/2024
+#   Version: 1.0
+#   Fecha: 15/11/2024
 #
-# NOTA: Por el momento el programa no tienen deteccion de errores, por lo que los
-# datos se deben ingresar correctamente, de lo contrario se debera volver a ejecutar
-# dicho programa.
+#   NOTA: Por el momento el programa no tienen deteccion de errores, por lo que los
+#   datos se deben ingresar correctamente, de lo contrario se debera volver a
+#   ejecutar dicho programa.
 #
 # ###################################################################################
 
